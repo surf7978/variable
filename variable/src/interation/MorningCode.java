37df2ec9914caa5649166b102d59cc9a2755e345
admin <admin@YD03-11>
2020-11-17 10:02:38 +0900

    11.17

--- variable/src/interation/MorningCode.java
+++ variable/src/interation/MorningCode.java
@@ -16,10 +16,10 @@ public class MorningCode
 	    	if(a % 7 == 0) {sum7 += a;}
 	    	else if(a % 8 == 0) {sum8 += a;}
 	    }
-	    System.out.println(sum7);
-	    System.out.println(7+14+21+28+35+42+49);
-	    System.out.println(sum8);
-	    System.out.println(8+16+24+32+40+48);
+	    System.out.println("for문으로 만듬");
+		System.out.println("현재 a값 : "+a);
+	    System.out.println("현재 sum7값 : "+sum7);
+	    System.out.println("현재 sum8값 : "+sum8);
 	    System.out.println("-------------");
 	    
 	    a=0;
@@ -28,15 +28,18 @@ public class MorningCode
 	    
 	    while(true) 
 	    {
-	    	a++;
-	    	if(a % 7 == 0) {sum7 += a;}
+	    	if(a>55) {break;}
+	    	else if(a % 7 == 0) {sum7 += a;}
 	    	else if(a % 8 == 0) {sum8 += a;}
-	    	else if(a==56) 
-	    	{
-	    		break;
-	    	}
+	    	a++;
 	    }
-	    System.out.println(sum7);
-	    System.out.println(sum8);	    
+	    System.out.println("while문으로 만듬");
+		System.out.println("현재 a값 : "+a);
+	    System.out.println("현재 sum7값 : "+sum7);
+	    System.out.println("현재 sum8값 : "+sum8);
+		System.out.println("-------------");
+		System.out.println("a값 56 에서 종료해야됨");
+	    System.out.println("sum7값 "+(7+14+21+28+35+42+49)+" 나와야 함");
+	    System.out.println("sum8값 "+(8+16+24+32+40+48)+" 나와야 함");
 	}
 }
