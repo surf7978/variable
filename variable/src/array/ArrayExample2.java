c7b1edcf90221f7d499778b0724dd719b6a700cc
admin <admin@YD03-11>
2020-11-18 14:01:05 +0900

    1118-1

--- variable/src/array/ArrayExample2.java
+++ variable/src/array/ArrayExample2.java
@@ -5,56 +5,62 @@ import java.security.ProtectionDomain;
 public class ArrayExample2 
 {
 
-//	public static void main(String[] args) 
-//	{
-//		double result = 0;
-//		double[] dAry;
-//		dAry = new double[] {23.2, 55.3, 52.8, 62.8};
-//		result = doubleSum(dAry);
-//		System.out.println("결과값: "+result);
-//
-//	}
-//	
-//	// double [] => 23.2, 55.3, 52.8, 62.8 : dAry로 선언하고
-//	// method: doubleSum(), 매개변수: double[]
-//	// 반환해주는 값: double
-//	
-//	public static double doubleSum(double[] a) 
-//	{
+	public static void main(String[] args) 
+	{
+		double result = 0;
+		double[] dAry;
+		dAry = new double[] {23.2, 55.3, 52.8, 62.8};
+		
+		//택1
 //		double sum =0;
-//		for(double i : a) 
+//		for(int i=0; i<dAry.length; i++) 
 //		{
-//			sum += i;
+//			sum += dAry[i];
 //		}
-//		return sum;
-	
-// 더 좋은방법으로 풀기	
+//		System.out.println("결과값: "+sum);
+		
+		
+		//택2
+		result = doubleSum(dAry);
+		System.out.println("결과값: "+result);
+
+	}
 	
 	// double [] => 23.2, 55.3, 52.8, 62.8 : dAry로 선언하고
 	// method: doubleSum(), 매개변수: double[]
 	// 반환해주는 값: double
-	public static void main(String[] args) 
+	
+	public static double doubleSum(double[] a) 
 	{
-		double result = 0;
-		double[] dAry =  {23.2, 55.3, 52.8, 62.8};
-		result = doubleSum(dAry);
-		System.out.println("결과값: "+result);
-	}
-	public static double doubleSum(double a[]) 
-	{
-		double result =0;
-		for(int i=0; i<a.length; i++)
+		double sum =0;
+		for(double i : a) 
 		{
-			result += a[i];
+			sum += i;
 		}
-		return result;
+		return sum;
+	
+// 더 좋은방법으로 풀기	
+	
+//	// double [] => 23.2, 55.3, 52.8, 62.8 : dAry로 선언하고
+//	// method: doubleSum(), 매개변수: double[]
+//	// 반환해주는 값: double
+//	public static void main(String[] args) 
+//	{
+//		double result = 0;
+//		double[] dAry =  {23.2, 55.3, 52.8, 62.8};
+//		result = doubleSum(dAry);
+//		System.out.println("결과값: "+result);
+//	}
+//	public static double doubleSum(double a[]) 
+//	{
+//		double result =0;
+//		for(int i=0; i<a.length; i++)
+//		{
+//			result += a[i];
+//		}
+//		return result;
+//	}
+//	//이건 왜 안될지 생각해보기
+//	
 	}
-	}
-	
-	
-	
-	
-	
-	
-	
 }
