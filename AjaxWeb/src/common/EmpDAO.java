8d9724d7d5d9d521b32d4d0f1578edab4e7c851b
surf7978 <surf7978@naver.com>
2021-01-05 15:22:07 +0900

    01.05

--- AjaxWeb/src/common/EmpDAO.java
+++ AjaxWeb/src/common/EmpDAO.java
@@ -2,7 +2,11 @@ package common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpDAO {
 	Connection conn = null;
@@ -25,7 +29,34 @@ public class EmpDAO {
 		}
 	} // end of 생성자
 	
-	public void getEmpList() {
-		
+	public List<EmployeeVO> getEmpList() {
+		String sql = "select * from employees";
+		List<EmployeeVO> list = new ArrayList<>();
+		try {
+			PreparedStatement psmt =  conn.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			while(rs.next()) {
+				EmployeeVO vo = new EmployeeVO();
+				vo.setEmployeeId(rs.getInt("employee_id"));
+				vo.setFirstName(rs.getString("first_name"));
+				vo.setLastName(rs.getString("last_name"));
+				vo.setEmail(rs.getString("email"));
+				vo.setPhoneNumber(rs.getString("phone_number"));
+				vo.setHireDate(rs.getString("hire_date"));
+				vo.setJobId(rs.getString("job_id"));
+				vo.setSalary(rs.getInt("salary"));
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close(); //이거 계속 켜두면 가끔 접속에러뜸 할때마다 접속꺼줘야 함
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
-}
+	
+} //end of class
--- AjaxWeb/src/common/HomeServlet.java
+++ AjaxWeb/src/common/HomeServlet.java
@@ -1,6 +1,8 @@
 package common;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,34 +15,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public HomeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setCharacterEncoding("utf-8");//한글출력용1
+		//response.setContentType("text/html; charset=utf-8"); //한글출력용2 꺽쇠(<)없애줘야함
+		
+		
 		// TODO Auto-generated method stub
 		EmpDAO dao = new EmpDAO();
 		String xml = "<dataset>";
-		xml += "<record><id>1</id><first_name>kildong</first_name><last_name>Hong</last_name></record>";
-		xml += "</dataset>";//데이터 넣어줌
-		
+		List<EmployeeVO> list = dao.getEmpList();
+		for (EmployeeVO emp : list) {
+			xml += "<record>";
+			xml += "<empId>" + emp.getEmployeeId() + "</empId>"//
+					+ "<firstName>" + emp.getFirstName() + "</firstName>"//
+					+ "<lastName>" + emp.getLastName() + "</lastName>"//
+					+ "<email>" + emp.getEmail() + "</email>"//
+					+ "<phoneNumber>" + emp.getPhoneNumber() + "</phoneNumber>"//
+					+ "<hireDate>" + emp.getHireDate() + "</hireDate>"//
+					+ "<jobId>" + emp.getJobId() + "</jobId>"//
+					+ "<salary>" + emp.getSalary() + "</salary>";
+			xml += "</record>";
+		}
+		xml += "</dataset>";// 데이터 넣어줌
+
 		response.getWriter().append(xml);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);//어짜피 doGet이 실행됨
+		doGet(request, response);// 어짜피 doGet이 실행됨
 	}
 
 }
